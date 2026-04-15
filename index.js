a8e70b010aa2f395ca4fcbd44073c3f72d8258ea
Alphine <alphinemkorea3@gmail.com>
2026-04-15 10:37:53 +0300

    fixed

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-
-
-
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+require('datejs')
+
+function combineUsers(...args){
+
+  let combinedObject = {
+    users : []
+  }
+
+  for(let arr of args){
+    combinedObject.users = [...combinedObject.users, ...arr]
+  }
+
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy')
+
+  return combinedObject
+}
